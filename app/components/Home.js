cab25352be62dfbc2a7f0c1c560ac2f31ef76b57
Martin Ek <mail@ekmartin.com>
2017-02-11 14:18:01 -0500

    css -> sass

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import styles from './Home.css';
 
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-        <div className={styles.container}>
+        <div>
           Hello World!
         </div>
       </div>
--- webpack.config.development.js
+++ webpack.config.development.js
@@ -28,22 +28,7 @@ export default validate(merge(baseConfig, {
 
   module: {
     loaders: [
-      {
-        test: /\.global\.css$/,
-        loaders: [
-          'style-loader',
-          'css-loader?sourceMap'
-        ]
-      },
-
-      {
-        test: /^((?!\.global).)*\.css$/,
-        loaders: [
-          'style-loader',
-          'css-loader?modules&sourceMap&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
-        ]
-      },
-
+      { test: /\.scss$/, loader: 'style!css!sass' },
       { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
       { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
