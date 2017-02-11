e858223a1c4886b38967eb8edd33e395d1bbe3a6
Eric Gardner <gardner.ec@gmail.com>
2017-02-11 15:46:34 -0500

    Build out basic bookdetails component

---
+++ app/components/BookDetails/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './BookDetails.scss';
+
+export default class BookDetails extends Component {
+  render() {
+    return (
+      <div className="book-details">
+        <div className="book-details__cover">
+          <img src={this.props.coverUrl} className="book-details__cover__image" />
+        </div>
+        <div className="book-details__info">
+          <h1 className="book-details__info__title">Moby Dick</h1>
+          <h2 className="book-details__info__author">Herman Melville</h2>
+          <h4 className="book-details__info__meta">Published by Project Gutenberg</h4>
+          <p className="book-details__info__description">
+            Nulla facilisi. Donec eros erat, molestie et dignissim in, pulvinar
+            facilisis risus. Ut at nisl vitae odio malesuada ultrices vitae sit
+            amet est. Cras feugiat neque sit amet elementum accumsan. Vestibulum
+            pharetra a neque nec congue. Maecenas vel sollicitudin tortor, nec
+            mattis mi. Praesent dui lorem, faucibus vitae purus semper, rhoncus
+            tincidunt magna.
+          </p>
+          <p className="book-details__info__subjects"></p>
+        </div>
+      </div>
+    );
+  }
+}
--- app/components/Home.js
+++ app/components/Home.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
+import BookDetails from './BookDetails/index.js';
 
+const coverUrl = 'https://raw.githubusercontent.com/GITenberg/Adventures-of-Huckleberry-Finn_76/master/cover.jpg';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-        <div>
-          Hello World!
-        </div>
+        <BookDetails coverUrl={coverUrl} />
       </div>
     );
   }
