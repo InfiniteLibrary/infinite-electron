e035a97e22975873cebc81e434cb2c4c0d05fd9d
Eric Gardner <gardner.ec@gmail.com>
2017-02-11 17:48:27 -0500

    Reorganize nav, fix to top
    Reorganizes the nav to a 3-part tabbed interface. There is also a home log on
    the left (infinity sign) and a placeholder about link on the right.
    
    Bulma doesn't support fixed position nav elements by default so this also
    includes a hack to override default behavior for .nav.is-fixed elements.

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -3,12 +3,19 @@ import Search from '../Search';
 import './Header.scss';
 
 const Header = () => (
-  <header className="Header__container">
-    <h1 className="Header__title">
-      Infinite Library
-    </h1>
-    <Search />
-  </header>
+  <nav className="nav has-shadow is-fixed">
+    <div className="nav-left">
+      <a className="header__logo" href="">∞</a>
+    </div>
+    <div className="nav-center">
+      <a className="nav-item is-tab is-active" href="">My Books</a>
+      <a className="nav-item is-tab" href="">Featured Titles</a>
+      <a className="nav-item is-tab" href="">Search for Books</a>
+    </div>
+    <div className="nav-right">
+      <a className="nav-item header__help-link is-tab" href="">About</a>
+    </div>
+  </nav>
 );
 
 export default Header;
