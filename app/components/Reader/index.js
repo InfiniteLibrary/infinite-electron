a8c3d278ea38027e04b46ad9c543050be333621f
Martin Ek <mail@ekmartin.com>
2017-02-11 20:45:48 -0500

    Add a basic epub component

--- app/components/Reader/index.js
+++ app/components/Reader/index.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import ePub from 'epubjs';
 
 class Reader extends Component {
+  componentWillMount() {
+    const { id } = this.props.book;
+    this.reader = ePub(`http://localhost:3300/${id}`);
+  }
+
   render() {
+    console.log(this.reader);
     return (
       <div>hi</div>
     );
