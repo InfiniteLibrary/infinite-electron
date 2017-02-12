eb2ca1bea30a3660c83b51004f38d4c1cc71fa0d
Martin Ek <mail@ekmartin.com>
2017-02-11 19:21:43 -0500

    Find correct book in BookDetails

--- app/containers/BookDetailsPage.js
+++ app/containers/BookDetailsPage.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 import BookDetails from '../components/BookDetails';
 
-const book = {
-  title: 'Anna Karenina',
-  author: 'Leo Tolstoy',
-  coverUrl: 'https://cdn.rawgit.com/plympton/rtc/master/rtc_books_resized/rtc_Anna+Karenina_Victoria+Fernandez.jpeg'
-};
-
 export default class BookDetailsPage extends Component {
   render() {
+    const selected = this.props.books.find(book => book.id === Number(this.props.params.bookId));
+    console.log(selected);
     return (
-      <BookDetails book={book} />
+      <BookDetails book={selected} />
     );
   }
 }
