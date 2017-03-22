25d74161bcc007100d3c129057e6fcf5cbad6ef1
eric <eric@hellman.net>
2017-03-22 11:01:38 -0400

    updated OPDS-json to latest version

--- rewrite.js
+++ rewrite.js
@@ -8,14 +8,22 @@ const { publications } = require(`./${process.argv[2]}`);
 const sanitizeHtml = require('sanitize-html');
 const { omit } = require('lodash');
 
-const findLinks = links => {
-  const image = links['opds:image'];
-  const download = links['opds:acquisition:open-access'];
+const findDownload = acquire => {
+  const download = acquire.find(target => target.type.includes('epub'));
   const result = {};
-  if (image) {
-    const imageUrl = image
-      .href
-      .replace('&zoom=1', '');
+
+  if (download) {
+      result.download = download.href;
+  }
+
+  return result;
+};
+
+const findImage = images => {
+  const result = {};
+  if (images && images.length){
+    const image = images.pop();
+    const imageUrl = image.href;
 
     // Make sure this is an actual URL (not local):d
     if (!imageUrl.startsWith('/')) {
@@ -23,31 +31,22 @@ const findLinks = links => {
     }
   }
 
-  if (download && download.length) {
-    const epub = download.find(target => target.type.includes('epub'));
-    if (epub) {
-      result.download = epub
-        .href
-        .replace('http://localhost:8000', 'https://unglue.it');
-    }
-  }
-
   return result;
 };
 
-const rewritten = publications
+var rewritten = publications
   .map(publication => {
-    const links = findLinks(publication._links);
+    const image = findImage(publication.images);
+    const download = findDownload(publication.acquire);
     // id is a url, and the actual number is the last part of it:
-    const idParts = publication.id.split('/');
+    const idParts = publication.metadata.id.split('/');
     const id = Number(idParts[idParts.length - 2]);
     return Object.assign({},
-      omit(publication, 'Rating', '_links', 'contributor'),
-      links,
+      download, image, omit(publication.metadata, '\@type', 'summary',  'category',  'rating'),
       {
         id,
-        summary: sanitizeHtml(publication.summary),
-        author: publication.contributor.name,
+        summary: sanitizeHtml(publication.metadata.summary),
+        rating: parseInt(publication.metadata.rating.ratingValue),
         // Randomly select books as own:
         isOwn: Math.random() > 0.993
       }
@@ -55,4 +54,5 @@ const rewritten = publications
   })
   .filter(publication => publication.download);
 
+rewritten.sort(function(a, b) {return b.rating - a.rating;});
 process.stdout.write(JSON.stringify(rewritten));
